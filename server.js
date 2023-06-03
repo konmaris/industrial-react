10e91317e2ec334f6e9092332888b5cd299b25a6
konmaris <mariskon02@gmail.com>
2023-06-03 21:07:18 +0300

    add

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const app = express();
 
 const path = require("path");
 
-const helmet = require("helmet");
-
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.use(helmet());
 app.disable("x-powered-by");
 
 const port = process.env.PORT || 3000;
